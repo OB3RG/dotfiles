1f274ede0603c8430523026444deee3cb9763d00
Oskar Öberg <oskar@shimayama.io>
2024-08-09 10:13:06 +0200

    update packages and theme

--- config/nvim/lua/community.lua
+++ config/nvim/lua/community.lua
@@ -6,7 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
 
   -- import/override with your plugins folder
 }
