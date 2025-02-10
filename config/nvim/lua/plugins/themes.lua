2869aae27216cf2a847b9d134b8c2d3ca053d659
Oskar Öberg <oskar.oberg@instabee.com>
2025-02-10 10:47:47 +0100

    replaced nvim config

---
+++ config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,9 @@
+-- Themes
+return {
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {},
+    }
+}
---
+++ config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+    config = function()
+      local wk = require("which-key")
+      wk.add({
+        {"<leader>f", group = "File"},
+        {"<leader>d", group = "Delete/Close"},
+        {"<leader>q", group = "Quit"},
+        {"<leader>s", group = "Search"},
+        {"<leader>l", group = "LSP"},
+        {"<leader>u", group = "UI"},
+        {"<leader>b", group = "Debugging"},
+        {"<leader>g", group = "Git"},
+      })
+    end
+  }
+}
