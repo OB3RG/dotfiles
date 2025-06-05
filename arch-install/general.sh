7b09c8fea39f69904f35012ed517bdc13b2832ee
Oskar Öberg <contact@oskaroberg.com>
2025-06-05 23:07:05 +0200

    add more packages

--- arch-install/general.sh
+++ arch-install/general.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pacman -S --noconfirm firefox thunar thunar-volman thunar-shares-plugin gvfs libsecret gnome-keyring gcr-4 blueman
+pacman -S --noconfirm firefox thunar thunar-volman thunar-shares-plugin gvfs libsecret gnome-keyring gcr-4 blueman obsidian syncthing
+
 
