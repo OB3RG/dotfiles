46bf41b046f34e8f1c5e1f5132252a431066e3de
Oskar Öberg <contact@oskaroberg.com>
2025-06-08 11:35:56 +0200

    add

--- arch-install/dev.sh
+++ arch-install/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pacman -S --noconfirm alacritty neovim zsh fzf go ripgrep docker
+pacman -S --noconfirm alacritty neovim zsh fzf go tmux ripgrep docker
 
 systemctl enable docker.service
