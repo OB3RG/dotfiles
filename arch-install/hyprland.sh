e542bbcbb631fcc3c6e8845c07a013403bff8c39
Oskar Öberg <contact@oskaroberg.com>
2025-09-12 23:34:57 +0200

    fix

--- arch-install/hyprland.sh
+++ arch-install/hyprland.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pacman -S --noconfirm hyprland hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland sddm waybar otf-font-awesome 
+pacman -S --noconfirm hyprland hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland sddm waybar otf-font-awesome wofi
 
 systemctl enable sddm
